ae1775c9ff5108754720c867bb43526640a9cd87
anetrebsky <alex.netrebsky@castle.co>
2016-05-20 14:19:37 +0400

    Add a post api/messages

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,4 +2,8 @@ class Api::MessagesController < ApplicationController
   def index
     render json: {:text => "you said #{params[:text]}"}
   end
+
+  def create
+    render json: {:text => "you said #{params[:text]}"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     get "messages", to: "messages#index"
+    post "messages", to: "messages#create"
   end
 end
